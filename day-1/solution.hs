32e36014d4a0c81bc23603e4ac95222e7388472c
Tameem Bin Haider <haidertameem@gmail.com>
2025-12-02 20:28:09 +0600

    feat(day-1): part 2

--- day-1/solution.hs
+++ day-1/solution.hs
@@ -1,14 +1,27 @@
 module Main where
 
 operate :: Integer -> String -> Integer
-operate current ('L': numStr) =
+operate current ('L' : numStr) =
   let num = read numStr :: Integer
-  in (current - num + 100) `mod` 100
-operate current ('R': numStr) =
+   in (current - num + 100) `mod` 100
+operate current ('R' : numStr) =
   let num = read numStr :: Integer
-  in (current + num) `mod` 100
+   in (current + num) `mod` 100
 
 partOne = length . filter (== 0) . scanl operate 50 . lines
 
+partTwo = sum . map snd . scanl operateWithCrossings (50, 0) . lines
+ where
+  operateWithCrossings (current, _) operation@('L' : numStr) =
+    let num = read numStr :: Integer
+        newCurrent = operate current operation
+        crossings = num `div` 100 + if current > 0 && current - (num `mod` 100) < 0 then 1 else 0
+     in (newCurrent, crossings + if newCurrent == 0 then 1 else 0)
+  operateWithCrossings (current, crossings) operation@('R' : numStr) =
+    let num = read numStr :: Integer
+        newCurrent = operate current operation
+        crossings = num `div` 100 + if current + (num `mod` 100) > 100 then 1 else 0
+     in (newCurrent, crossings + if newCurrent == 0 then 1 else 0)
+
 main :: IO ()
-main = interact $ show . partOne
+main = interact $ show . partTwo
