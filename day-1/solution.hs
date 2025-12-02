e12f14de2f138e6d2109a3502da7bf70045c31d1
Tameem Bin Haider <haidertameem@gmail.com>
2025-12-02 14:34:14 +0600

    feat(day-1): part 1

--- day-1/solution.hs
+++ day-1/solution.hs
@@ -1,4 +1,14 @@
 module Main where
 
+operate :: Integer -> String -> Integer
+operate current ('L': numStr) =
+  let num = read numStr :: Integer
+  in (current - num + 100) `mod` 100
+operate current ('R': numStr) =
+  let num = read numStr :: Integer
+  in (current + num) `mod` 100
+
+partOne = length . filter (== 0) . scanl operate 50 . lines
+
 main :: IO ()
-main = interact id
+main = interact $ show . partOne
